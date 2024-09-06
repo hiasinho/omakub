9c1a82f7c39670d372527d73d0ffb8f8c407481e
Mathias Maisberger <me@hiasinho.com>
2024-09-06 21:49:49 +0200

    lazyvim optional

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -7,25 +7,6 @@ sudo cp -R nvim-linux64/share /usr/local/
 rm -rf nvim-linux64 nvim.tar.gz
 cd -
 
-# Only attempt to set configuration if Neovim has never been run
-if [ ! -d "$HOME/.config/nvim" ]; then
-	# Use LazyVim
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
-
-	# Disable update notification popup in starter config
-	sed -i 's/checker = { enabled = true }/checker = { enabled = true, notify = false }/g' ~/.config/nvim/lua/config/lazy.lua
-
-	# Make everything match the terminal transparency
-	mkdir -p ~/.config/nvim/plugin/after
-	cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
-
-	# Default to Tokyo Night theme
-	cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
-
-	# Enable default extras
-	cp ~/.local/share/omakub/configs/neovim/lazyvim.json ~/.config/nvim/lazyvim.json
-fi
-
 # Replace desktop launcher with one running inside Alacritty
 if [[ -d ~/.local/share/applications ]]; then
 	sudo rm -rf /usr/share/applications/nvim.desktop
