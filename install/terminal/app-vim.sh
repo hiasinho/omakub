1faaaf4b48f60c2e4ab970550250a7413b454f22
Mathias Maisberger <me@hiasinho.com>
2024-09-07 11:49:10 +0200

    update vim

--- install/terminal/app-vim.sh
+++ install/terminal/app-vim.sh
@@ -3,3 +3,6 @@ sudo apt install -y vim-gtk3
 [ -f "~/.vimrc" ] && mv ~/.vimrc ~/.vimrc.bak
 # cp ~/.local/share/omakub/configs/vimrc ~/.vimrc
 ln -sf ~/.local/share/omakub/configs/vimrc ~/.vimrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
