24534beda494bb046719761dc511ccbe3dbd4a31
Mathias Maisberger <me@hiasinho.com>
2024-09-07 11:46:51 +0200

    update tmux

--- install/terminal/app-tmux.sh
+++ install/terminal/app-tmux.sh
@@ -1,3 +1,6 @@
 sudo apt install -y tmux
 
+mkdir -p ~/.config/tmux
 ln -sf ~/.local/share/omakub/configs/tmux.conf ~/.config/tmux/tmux.conf
+
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
